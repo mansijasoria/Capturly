41792883f86e49cf8693925c8efb446a6aff50b4
Mansi Jasoria <mansijasoria13@gmail.com>
2025-06-09 23:06:45 +0530

    Video Upload Logic added need to fix!

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -2,13 +2,39 @@
 import FileInput from "@/components/FileInput";
 import FormField from "@/components/FormField";
 import { MAX_THUMBNAIL_SIZE, MAX_VIDEO_SIZE } from "@/constants";
+import {
+  getThumbnailUploadUrl,
+  getVideoUploadUrl,
+  saveVideoDetails,
+} from "@/lib/actions/video";
 import { useFileInput } from "@/lib/hooks/useFileInput";
-import { title } from "process";
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+
+const uploadFileToBunny = (
+  file: File,
+  uploadUrl: string,
+  accessKey: string
+): Promise<void> =>
+  fetch(uploadUrl, {
+    method: "PUT",
+    headers: {
+      "Content-Type": file.type,
+      AccessKey: accessKey,
+    },
+    body: file,
+  }).then((response) => {
+    if (!response.ok)
+      throw new Error(`Upload failed with status ${response.status}`);
+  });
+
 
 const page = () => {
-
+  const router = useRouter();
   const [isSubmitting, setSubmitting] = useState(false);
+
+  const [videoDuration, setVideoDuration] = useState(0);
 
   const [formData, setFormData] = useState({
     title: "",
@@ -19,6 +45,12 @@ const page = () => {
   const video = useFileInput(MAX_VIDEO_SIZE);
   const thumbnail = useFileInput(MAX_THUMBNAIL_SIZE);
 
+  useEffect(() => {
+    if (video.duration != null || 0) {
+      setVideoDuration(video.duration);
+    }
+  }, [video.duration]);
+
   const [error, setError] = useState("");
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -27,34 +59,70 @@ const page = () => {
     setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const handleSubmit = async(e:FormEvent) =>{
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
     setSubmitting(true);
 
     try {
-      if(!video.file || !thumbnail.file){
-        setError('Please upload video and thumbnail');
+      if (!video.file || !thumbnail.file) {
+        setError("Please upload video and thumbnail");
         return;
       }
-      if(!formData.title || !formData.description){
-        setError('Please fill in all the details');
-         return;
+      if (!formData.title || !formData.description) {
+        setError("Please fill in all the details");
+        return;
       }
+    
+       // TODO
+        // Upload Video to bunny
+        //step 1: get the upload url
+        const {
+          videoId,
+          uploadUrl: videoUploadUrl,
+          accessKey: videoAccessKey,
+        } = await getVideoUploadUrl();
+
+        if (!videoUploadUrl || !videoAccessKey)
+          throw new Error("Failed to get a video upload credentials");
+
+        //step 2: upload the video to bunny
+
+        await uploadFileToBunny(video.file, videoUploadUrl, videoAccessKey);
+
+        // Upload the thumbnail to db
+        const {
+          uploadUrl: thumbnailUploadUrl,
+          accessKey: thumbnailAccessKey,
+          cdnUrl: thumbnailCdnUrl,
+        } = await getThumbnailUploadUrl(videoId);
+
+        if (!thumbnailUploadUrl || !thumbnailAccessKey || !thumbnailCdnUrl)
+          throw new Error("Failed to get a thumbnail upload credentials");
+
+        // attach thumbnail
+        await uploadFileToBunny(
+          thumbnail.file,
+          thumbnailUploadUrl,
+          thumbnailAccessKey
+        );
+
+        //Create a new DB enrtry for the video Details(url, data)
+        await saveVideoDetails({
+          videoId,
+          thumbnailUrl: thumbnailCdnUrl,
+          ...formData,
+          duration: videoDuration,
+        });
+
+        router.push(`/video/${videoId}`)
+  
     } catch (error) {
-      console.log('Error Submitting form', error);
-    } finally{
+      console.log("Error Submitting form", error);
+    } finally {
       setSubmitting(false);
-    };
-
-    // TODO
-    // Upload Video to bunny
-    // Upload the thumbnail to db
-    // attach thumbnail
-    //Create a new DB enrtry for the video Details(url, data)
-
-
-  }
+    }
+  };
   return (
     <div className="wrapper-md upload-page">
       <h1>Upload a video</h1>
@@ -88,7 +156,8 @@ const page = () => {
           inputRef={video.inputRef}
           onChange={video.handleFileChange}
           onReset={video.resetFile}
-          type="video" />
+          type="video"
+        />
 
         <FileInput
           id="thumbnail"
@@ -99,9 +168,10 @@ const page = () => {
           inputRef={thumbnail.inputRef}
           onChange={thumbnail.handleFileChange}
           onReset={thumbnail.resetFile}
-          type="image" />
+          type="image"
+        />
 
-          <FormField
+        <FormField
           id="visibility"
           label="Visibility"
           value={formData.visibility}
@@ -109,13 +179,13 @@ const page = () => {
           placeholder="Describe What this Video is about"
           as="select"
           options={[
-            {value: 'public', label: 'Public'},
-            {value: 'private', label: 'Private'},
+            { value: "public", label: "Public" },
+            { value: "private", label: "Private" },
           ]}
         />
 
         <button type="submit" disabled={isSubmitting} className="submit-button">
-          {isSubmitting?'Uploading...':'Upload video'}
+          {isSubmitting ? "Uploading..." : "Upload video"}
         </button>
       </form>
     </div>
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -3,6 +3,8 @@ import {
   text,
   timestamp,
   boolean,
+  integer,
+  uuid,
 } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
@@ -65,7 +67,22 @@ export const verification = pgTable("verification", {
   ),
 });
 
-
+export const videos = pgTable("videos", {
+  id: uuid("id").primaryKey().defaultRandom().unique(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  videoUrl: text("video_url").notNull(),
+  videoId: text("video_id").notNull(),
+  thumbnailUrl: text("thumbnail_url").notNull(),
+  visibility: text("visibility").$type<"public" | "private">().notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  views: integer("views").notNull().default(0),
+  duration: integer("duration"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
 export const schema = {
     user,
     session,
